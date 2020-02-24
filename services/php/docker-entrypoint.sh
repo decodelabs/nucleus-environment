fdb770d30fa40303a58244b327aa433ed21b0787
Tom Wright <tom@inflatablecookie.com>
2020-02-24 17:10:40 +0000

    Added GIT config
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- services/php/docker-entrypoint.sh
+++ services/php/docker-entrypoint.sh
@@ -7,5 +7,9 @@ set -o pipefail
 rm -f /var/run/mysqld/mysqld.sock
 su -c "socat UNIX-LISTEN:/var/run/mysqld/mysqld.sock,fork TCP:mariadb:3306 &" -s /bin/bash nucleus
 
+# Setup git
+su -c "git config --global user.name \"${GIT_NAME}\"" -s /bin/sh nucleus
+su -c "git config --global user.email \"${GIT_EMAIL}\"" -s /bin/sh nucleus
+
 # Run FPM
 exec "$@"
