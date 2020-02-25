4e9ea4af77afd360b8e99ba840ca22a2a1107bae
Tom Wright <tom@inflatablecookie.com>
2020-02-25 02:15:30 +0000

    Added certificate generation
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- services/php/docker-entrypoint.sh
+++ services/php/docker-entrypoint.sh
@@ -11,5 +11,9 @@ su -c "socat UNIX-LISTEN:/var/run/mysqld/mysqld.sock,fork TCP:mariadb:3306 &" -s
 su -c "git config --global user.name \"${GIT_NAME}\"" -s /bin/sh nucleus
 su -c "git config --global user.email \"${GIT_EMAIL}\"" -s /bin/sh nucleus
 
+# Copy certificates
+mkdir -p /srv/env/nucleus/certificates/
+cp -R /srv/util/certificates/* /srv/env/nucleus/certificates/
+
 # Run FPM
 exec "$@"
