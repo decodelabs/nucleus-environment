51be53f1940b0ff6b4822bb96b80b78083311008
Tom Wright <tom@inflatablecookie.com>
2020-03-11 21:13:04 +0000

    Pipe nginx ports to php container
    Signed-off-by: Tom Wright <tom@inflatablecookie.com>

--- stacks/www/services/php/docker-entrypoint.sh
+++ stacks/www/services/php/docker-entrypoint.sh
@@ -6,6 +6,8 @@ set -o pipefail
 # Pipe mysql 3306 port to Unix socket
 rm -f /var/run/mysqld/mysqld.sock
 su -c "socat UNIX-LISTEN:/var/run/mysqld/mysqld.sock,fork TCP:mariadb:3306 &" -s /bin/bash ${USERNAME}
+socat TCP-LISTEN:80,fork TCP:nginx:80 &
+socat TCP-LISTEN:443,fork TCP:nginx:443 &
 
 # Setup git
 su -c "git config --global user.name \"${GIT_NAME}\"" -s /bin/sh ${USERNAME}
